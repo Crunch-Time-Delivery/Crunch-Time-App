30ea60e9490eed91640eec947a54042742826880
SamuariX <jdv853786@gmail.com>
2025-10-17 10:22:09 +0200

    added import connection.jsx in index.jsx

--- driver-app/index.jsx
+++ driver-app/index.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './DriverMap.jsx';
 import App from './app.jsx';
+import App from './Connection.jsx';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
--- vendor-app/index.jsx
+++ vendor-app/index.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './app.jsx';
 import App from './createPayFastPayment.jsx';
 import App from './ManagementSection.jsx';
+import App from './Connection.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
