f6b3b2e1f3d266ab22890341c36c368ffbd40217
SamuariX <jdv853786@gmail.com>
2025-11-12 13:34:07 +0200

    map.js file addded by driver side

---
+++ driver-app/client/src/server/map.js
@@ -0,0 +1,7 @@
+var map;
+var mark;
+var initialize = function() {
+  map  = new google.maps.Map(document.getElementById('map-canvas'), {center:{lat:lat,lng:lng},zoom:12});
+  mark = new google.maps.Marker({position:{lat:lat, lng:lng}, map:map});
+};
+window.initialize = initialize;
--- driver-app/liveTracking.js
+++ driver-app/liveTracking.js
@@ -1,12 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 
-var map;
-var mark;
-var initialize = function() {
-  map  = new google.maps.Map(document.getElementById('map-canvas'), {center:{lat:lat,lng:lng},zoom:12});
-  mark = new google.maps.Marker({position:{lat:lat, lng:lng}, map:map});
-};
-window.initialize = initialize;
+
 
 function LiveTracking({ userLat, userLng, onLocationChange }) {
   const mapContainerRef = useRef(null);
