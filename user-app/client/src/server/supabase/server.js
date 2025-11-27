b97695ce4405f5bfe201a44c8669e01e0114d163
SamuariX <jdv853786@gmail.com>
2025-11-27 11:49:13 +0200

    update

--- user-app/client/src/server/supabase/server.js
+++ user-app/client/src/server/supabase/server.js
@@ -1,7 +1,8 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+
+const supabaseUrl = 'https://wbpgmgtoyzlnawvsfeiu.supabase.co';
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 // Create the client with cookie management
