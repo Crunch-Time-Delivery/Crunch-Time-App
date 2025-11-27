d691e66e9b30b03c4312af067982e97ecd6318c8
SamuariX <jdv853786@gmail.com>
2025-11-27 10:49:44 +0200

    update

--- driver-app/client/src/server/supabase/client.js
+++ driver-app/client/src/server/supabase/client.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseUrl = 'https://wbpgmgtoyzlnawvsfeiu.supabase.co';
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const createClient = () => createClient(supabaseUrl, supabaseKey);
