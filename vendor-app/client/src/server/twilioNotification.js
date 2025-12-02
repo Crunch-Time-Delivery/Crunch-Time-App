8fbed158a24e9933a9b726d730588116f24cecb9
SamuariX <jdv853786@gmail.com>
2025-12-02 10:47:16 +0200

    update

--- vendor-app/client/src/server/twilioNotification.js
+++ vendor-app/client/src/server/twilioNotification.js
@@ -8,8 +8,7 @@ const accountSid =  AC031642049dd74fcc581b0fd106936a4f;
 const authToken = "1447e415a2fc483bd2bfbea57451d55d";
 const client = twilio(accountSid, authToken);
 
-/**
- * Send SMS via Twilio
+/*** Send SMS via Twilio
  * @param {string} to - The recipient phone number
  * @param {string} message - The message body
  * @returns {Promise} - Promise resolving to message info
