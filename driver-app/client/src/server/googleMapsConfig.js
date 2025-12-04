b531013dbc8a23869457b9364123c3855d4b21a8
SamuariX <jdv853786@gmail.com>
2025-12-04 11:34:23 +0200

    update

--- driver-app/client/src/server/googleMapsConfig.js
+++ driver-app/client/src/server/googleMapsConfig.js
@@ -4,7 +4,7 @@ async function initGoogleMap() {
 
   // Load Google Maps script dynamically
   const script = document.createElement('script');
-  script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}`;
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${AIzaSyB9sNhi824hNncjfW7HHzaI_s8JtWGfM0Q}`;
   document.head.appendChild(script);
   script.onload = () => {
     const map = new google.maps.Map(document.getElementById('map'), mapOptions);
