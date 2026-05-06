1e64d012fc9bbb7a5b4b3b1212cf3113cfafb537
SamuariX <jdv853786@gmail.com>
2026-05-06 14:47:46 +0200

    feat(update):update code of abel.config.js

--- admin-react-app/abel.config.js
+++ admin-react-app/abel.config.js
@@ -1,11 +1,16 @@
 module.exports = function (api) {
+  // Enable caching for faster rebuilds
   api.cache(true);
 
-  const presets = ["@babel/preset-env"];
-  const plugins = ["@babel/plugin-proposal-class-properties"];
+  // Define Babel presets
+  const presets = ['@babel/preset-env'];
 
+  // Define Babel plugins
+  const plugins = ['@babel/plugin-proposal-class-properties'];
+
+  // Return configuration object
   return {
     presets,
-    plugins
+    plugins,
   };
 };
